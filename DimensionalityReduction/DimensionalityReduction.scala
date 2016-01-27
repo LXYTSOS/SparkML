c8f9151d69d4514164ec9a8a2f353505dbce7b88
LXYTSOS <834287447@qq.com>
2016-01-27 11:33:22 +0800

    比较PCA&SVD，评估降维模型，评估SVD前300个奇异值

--- DimensionalityReduction/DimensionalityReduction.scala
+++ DimensionalityReduction/DimensionalityReduction.scala
@@ -10,6 +10,7 @@ import org.apache.spark.SparkConf
 import org.apache.spark.mllib.linalg.distributed.RowMatrix
 import breeze.linalg.DenseMatrix
 import breeze.linalg.csvwrite
+import breeze.linalg.DenseVector
 
 /**
  * @author sl169
@@ -65,16 +66,55 @@ object DimensionalityReduction {
     val matrix = new RowMatrix(scaledVectors)
 //    println(matrix.numRows(),matrix.numCols())
     //(1054, 2500)
-    val k = 10
-    val pc = matrix.computePrincipalComponents(k)
+//    val k = 10
+    //这个步骤比较耗时（大概5分钟），比SVD（1分钟左右）要慢许多
+//    val pc = matrix.computePrincipalComponents(k)
     
-    val rows = pc.numRows
-    val cols = pc.numCols
+//    val rows = pc.numRows
+//    val cols = pc.numCols
 //    println(rows, cols)
     //(2500,10)
     
-    val pcBreeze = new DenseMatrix(rows, cols, pc.toArray)
-    csvwrite(new File("F:/ScalaWorkSpace/data/pc.csv"), pcBreeze)
+    //Save principle components as csv file
+//    val pcBreeze = new DenseMatrix(rows, cols, pc.toArray)
+//    csvwrite(new File("F:/ScalaWorkSpace/data/pc.csv"), pcBreeze)
+    
+    //Projecting data using PCA on the FLW dataset
+//    val projected = matrix.multiply(pc)
+//    println(projected.numRows(),projected.numCols())
+    //(1054,10)
+//    println(projected.rows.take(5).mkString("\n"))
+    
+//    val svd = matrix.computeSVD(10, computeU = true)
+//    println(s"U dimension: (${svd.U.numRows}, ${svd.U.numCols})")
+//    println(s"S dimension: (${svd.s.size}, )")
+//    println(s"V dimension: (${svd.V.numRows}, ${svd.V.numCols})")
+    // U dimension: (1054, 10)
+    // S dimension: (10, )
+    // V dimension: (2500, 10)
+    
+//    println(approxEqual(svd.V.toArray, pc.toArray))
+    
+    //Compare U and S matrix with PCA projection
+//    val breezeS = DenseVector(svd.s.toArray)
+//    val projectedSVD = svd.U.rows.map{ v => 
+//      val breezeV = DenseVector(v.toArray)
+//      val multV = breezeV :* breezeS
+//      Vectors.dense(multV.data)
+//    }
+//    val result = projected.rows.zip(projectedSVD).map{ case (v1, v2) => 
+//      approxEqual(v1.toArray, v2.toArray)}.filter(b => true).count
+//    println(result)
+    //1054
+    
+    //Evaluating k for SVD on the LFW dataset
+//    val sValues = (1 to 5).map{ i => matrix.computeSVD(i, computeU = false).s}
+//    sValues.foreach(println)
+    
+    //计算前300个奇异值，并保存为csv文件，使用Python绘图观察图像从哪个点开始变平缓
+    val svd300 = matrix.computeSVD(300, computeU = false)
+    val sMatrix = new DenseMatrix(1,300, svd300.s.toArray)
+    csvwrite(new File("F:/ScalaWorkSpace/data/s300.csv"), sMatrix)
   }
   
   //Load an image
@@ -106,4 +146,13 @@ object DimensionalityReduction {
     val processed = processImage(raw, width, height)
     getPixelsFromImage(processed)
   }
+  
+  //Compare V matrix from svd with the result of PCA
+  def approxEqual(array1: Array[Double], array2: Array[Double], tolerance: Double = 1e-6): Boolean = {
+    val bools = array1.zip(array2).map{ case (v1, v2) => if
+      (math.abs(math.abs(v1) - math.abs(v2)) > 1e-6) false else true}
+    bools.fold(true)(_&_)
+  }
+  
+  
 }
