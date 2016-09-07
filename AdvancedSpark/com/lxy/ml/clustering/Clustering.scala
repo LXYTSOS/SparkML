ef030aa48929faebe0214154aaa5dffe91f5d9b5
LXYTSOS <834287447@qq.com>
2016-09-07 21:18:48 +0800

    计算数据点到簇心的平均距离

--- AdvancedSpark/com/lxy/ml/clustering/Clustering.scala
+++ AdvancedSpark/com/lxy/ml/clustering/Clustering.scala
@@ -2,8 +2,12 @@ package com.lxy.ml.clustering
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
-import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.clustering.KMeans
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
+import org.apache.spark.mllib.clustering.KMeansModel
+import org.apache.spark.rdd.RDD
 
 object Clustering {
   def main(args: Array[String]){
@@ -32,14 +36,36 @@ object Clustering {
 //    model.clusterCenters.foreach(println)
     
     //下面看下每个簇都有哪些类
-    val clusterLabelCount = labelsAndData.map{ case (label,datum) =>
-      val cluster = model.predict(datum)
-      (cluster, label)
-    }.countByValue
+//    val clusterLabelCount = labelsAndData.map{ case (label,datum) =>
+//      val cluster = model.predict(datum)
+//      (cluster, label)
+//    }.countByValue
+//    
+//    clusterLabelCount.toSeq.sorted.foreach{
+//      case ((cluster, label), count) =>
+//        println(f"$cluster%1s$label%18s$count%8s")
+//    }
     
-    clusterLabelCount.toSeq.sorted.foreach{
-      case ((cluster, label), count) =>
-        println(f"$cluster%1s$label%18s$count%8s")
-    }
+    (5 to 40 by 5).map(k => (k, clusteringScore(data, k))).foreach(println)
+  }
+  
+  //计算两个向量之间的距离
+  def distance(a: Vector, b: Vector) = {
+    math.sqrt(a.toArray.zip(b.toArray).map(p => p._1 - p._2).map(d => d*d).sum)
+  }
+  
+  //计算一个向量到所属簇心的距离
+  def distToCentroid(datum: Vector, model: KMeansModel) = {
+    val cluster = model.predict(datum)
+    val centroid = model.clusterCenters(cluster)
+    distance(centroid, datum)
+  }
+  
+  //计算数据点到簇心的平均距离
+  def clusteringScore(data: RDD[Vector], k: Int) = {
+    val kmeans = new KMeans()
+    kmeans.setK(k)
+    val model = kmeans.run(data)
+    data.map(datum => distToCentroid(datum, model)).mean()
   }
 }
